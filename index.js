98c93c3d2c37fde7931a01fd1cc9d4d40b191c2f
marcovicci <48192998+marcovicci@users.noreply.github.com>
2021-03-26 18:48:31 +1300

    tinkering

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-// index.js
-const { prefix, token } = require('./config.json');
-// ... client setup (keep reading)
-
 const Discord = require('discord.js');
 const Sequelize = require('sequelize');
 
 const client = new Discord.Client();
 const PREFIX = process.env.PREFIX;
-
-/*
- * Make sure you are on at least version 5 of Sequelize! Version 4 as used in this guide will pose a security threat.
- * You can read more about this issue On the [Sequelize issue tracker](https://github.com/sequelize/sequelize/issues/7310).
- */
 
 const sequelize = new Sequelize(DATABASE_URL, 'username', 'password', {
 	host: 'localhost',
@@ -74,6 +65,7 @@ client.once('ready', () => {
 	Users.sync();
 	Pets.sync();
 	console.log('hewwo')
+	client.user.setActivity('carefully', {type: 'WATCHING'});
 });
 
 client.on('message', async message => {
