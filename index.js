55eef550079045109a8ebb07601c434b27308d19
marcovicci <48192998+marcovicci@users.noreply.github.com>
2021-03-28 09:17:14 +1300

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ disclient.on('message', message => {
       const command = args.shift().toLowerCase();
 
       //keep the discord ID of the person who sent this message - we'll need it for basically all commands!
-      const verifyDiscordID = String(message.author.id);
+      const verifyDiscordID = message.author.id.toString();
 
       //this helped me with a lot of bug testing
       //console.log('cmd is ' + command);
