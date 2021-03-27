3636247de75ef33027bff927c22feca14fa47a71
marcovicci <48192998+marcovicci@users.noreply.github.com>
2021-03-27 13:42:48 +1300

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,5 @@
-const postgres = require('pg');
+const postgres = require('pg')
 
-const sql = postgres({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const sql = postgres({connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false }})
 
 module.exports = sql
