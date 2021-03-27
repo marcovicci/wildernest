2eebab9c8ec034c95dbae246f6abb5aef805e210
marcovicci <48192998+marcovicci@users.noreply.github.com>
2021-03-28 09:40:02 +1300

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function UserCreate(message, commanderName, verifyDiscordID) {
   		//console.log('user id exists as ' + checkUsers);
   		//If this check succeeds there is a user ID for this already, so let's do some more stuff.
   		try {
-        const matchDiscord = await sql.query(`SELECT discordid FROM users WHERE userid = ${checkUsers.userid}`);
+        const matchDiscord = await sql.query(`SELECT discordid FROM users WHERE userid = ${checkUsers.rows[0]}`);
 
         //Let's check if the discord account matches.
         if (matchDiscord != verifyDiscordID) {
@@ -59,14 +59,14 @@ async function UserCreate(message, commanderName, verifyDiscordID) {
     			return message.reply(`Sorry - I already know a ${commanderName} and their Discord ID is ${matchDiscord}, yours is ${verifyDiscordID}!`);
         } else {
     			//Discord ID matches. Cool. So, do you have a username already?
-          try { const matchUsername = await sql.query(`SELECT username FROM users WHERE userid = ${checkUsers.userid}`);
+          try { const matchUsername = await sql.query(`SELECT username FROM users WHERE userid = ${checkUsers.rows[0]}`);
           //yes!
           return message.reply(`Hi ${matchUsername}. You're already in my system, with the correct ID of ${verifyDiscordID}. Thanks for checking.`);
           } catch(err) { //nope, no username
           console.log('No username found so we give you a new one.');
            await sql.query(`
      		   UPDATE users SET username = ${commanderName}
-           WHERE userid = ${checkUsers.userid}
+           WHERE userid = ${checkUsers.rows[0]}
      			`);
            return message.reply(`Thanks, ${commanderName}! Your Discord ID has been added as ${verifyDiscordID}.`); }
         }
@@ -76,7 +76,7 @@ async function UserCreate(message, commanderName, verifyDiscordID) {
         //console.log('No discord ID found so we will write yours in.');
   			await sql.query(`
   		  UPDATE users SET discordid = ${verifyDiscordID}
-        WHERE userid = ${checkUsers.userid}
+        WHERE userid = ${checkUsers.rows[0]}
   			`);
   			return message.reply(`Thanks, ${commanderName}! Your Discord ID has been added as ${verifyDiscordID}.`);
       }
@@ -105,13 +105,13 @@ async function PetsCreate(message, args, verifyDiscordID) {
   console.log(verifyDiscordID);
   try {
     const checkUsers = await sql.query(`SELECT userid FROM users WHERE discordid = ${verifyDiscordID}`);
-    console.log(checkUsers.userid);
+    console.log(checkUsers.rows[0]);
     //we know this guy, let's execute the command
 
     if (!args.length) {
       //if no arguments let's fetch their Pets
         try {
-          const checkPets = await sql.query(`SELECT * FROM pets WHERE ownerid = ${checkUsers.userid}`);
+          const checkPets = await sql.query(`SELECT * FROM pets WHERE ownerid = ${checkUsers.rows[0]}`);
           console.log('pets are ' + checkPets);
           return message.reply(`You have some pets alright. (I'll be able to list them out later.)`);
         } catch(err) {
@@ -123,21 +123,21 @@ async function PetsCreate(message, args, verifyDiscordID) {
       //let's try to make a new pet!
       //but first, let's make sure the user has room
       try {
-        const currentPets = await sql.query(`SELECT totalpets FROM users WHERE userid = ${checkUsers.userid}`);
-        const allowedPets = await sql.query(`SELECT allowedpets FROM users WHERE userid = ${checkUsers.userid}`);
+        const currentPets = await sql.query(`SELECT totalpets FROM users WHERE userid = ${checkUsers.rows[0]}`);
+        const allowedPets = await sql.query(`SELECT allowedpets FROM users WHERE userid = ${checkUsers.rows[0]}`);
         if (allowedPets > currentPets) {
           //we can make a pet!
           try {
             await sql.query(`
         		  INSERT INTO pets (petname, ownerid)
-                VALUES ('${args[1]}', '${checkUsers.userid}')
+                VALUES ('${args[1]}', '${checkUsers.rows[0]}')
         		`);
 
             //if that worked, let's also update the current pets for that user
             try {
               await sql.query(`
         		  UPDATE users SET totalpets = totalpets + 1
-              WHERE userid = ${checkUsers.userid}
+              WHERE userid = ${checkUsers.rows[0]}
         			`);
             } catch(err) {
               //why didn't that work? let's see
