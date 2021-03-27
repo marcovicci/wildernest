0192415481512ed7a25f8666ba21363d76e8c5a1
marcovicci <48192998+marcovicci@users.noreply.github.com>
2021-03-28 09:07:56 +1300

    cleanup

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// Discord.js bot
+// Discord.js bot setup
 const Discord = require('discord.js');
-const Sequelize = require('sequelize');
-const config = require('./config/config.json');
 const disclient = new Discord.Client();
 
+//Config file and postgreSQL client setup
+const config = require('./config/config.json');
 const { Client } = require('pg');
 
 const sql = new Client({
@@ -13,40 +13,54 @@ const sql = new Client({
   }
 });
 
+//Sends to console once the bot is ready for interaction
 disclient.once('ready', () => {
 	console.log('hewwo');
 });
 
+//On a new message in a channel the bot has access to...
 disclient.on('message', message => {
-    //console.log('message is ' + message.content);
+    //it only cares abiut messages if they begin with its prefix value I set up on Heroku
     if (message.content.startsWith(process.env.PREFIX)) {
+
+      //split the message into arguments and commands
       const args = message.content.slice(process.env.PREFIX.length).trim().split(' ');
       const command = args.shift().toLowerCase();
+
+      //keep the discord ID of the person who sent this message - we'll need it for basically all commands!
       const verifyDiscordID = String(message.author.id);
-      console.log('cmd is ' + command);
-      console.log('args is ' + args);
+
+      //this helped me with a lot of bug testing
+      //console.log('cmd is ' + command);
+      //console.log('args is ' + args);
+
+      //in lieu of a sophisticated event handler i just have this block leading to some functions
       if (command === 'i\'m') UserCreate(message, args, verifyDiscordID);
       else if (command === 'pets') PetsCreate(message, args, verifyDiscordID);
     }
 });
 
+//async functions are the best for my purposes - being able to 'try' reading and writing to the SQL database was essential
 async function UserCreate(message, commanderName, verifyDiscordID) {
   sql.connect();
 	try {
     const checkUsers = await sql.query(`SELECT userid FROM users WHERE username = ${commanderName} OR discordid = ${verifyDiscordID}`);
 
-  		console.log('user id exists as ' + checkUsers);
-  		//There is a user ID for this already, so let's do some more stuff.
+  		//console.log('user id exists as ' + checkUsers);
+  		//If this check succeeds there is a user ID for this already, so let's do some more stuff.
   		try {
         const matchDiscord = await sql.query(`SELECT discordid FROM users WHERE userid = ${checkUsers}`);
+
         //Let's check if the discord account matches.
         if (matchDiscord != verifyDiscordID) {
-          console.log('desired discord ID is ' + matchDiscord);
+          //console.log('desired discord ID is ' + matchDiscord);
+
     			//This isn't your account, let's just yell at you.
     			return message.reply(`Sorry - I already know a ${commanderName} and their Discord ID is ${matchDiscord}, yours is ${verifyDiscordID}!`);
         } else {
     			//Discord ID matches. Cool. So, do you have a username already?
           try { const matchUsername = await sql.query(`SELECT username FROM users WHERE userid = ${checkUsers}`);
+          //yes!
           return message.reply(`Hi ${matchUsername}. You're already in my system, with the correct ID of ${verifyDiscordID}. Thanks for checking.`);
           } catch(err) { //nope, no username
           console.log('No username found so we give you a new one.');
@@ -58,7 +72,8 @@ async function UserCreate(message, commanderName, verifyDiscordID) {
         }
       } catch(err) {
   			//Well, there's no discord ID. For now, we'll just let them write theirs in.
-        console.log('No discord ID found so we will write yours in.');
+        //In the future, we'll require them to authorize on a web interface.
+        //console.log('No discord ID found so we will write yours in.');
   			await sql.query(`
   		  UPDATE users SET discordid = ${verifyDiscordID}
         WHERE userid = ${checkUsers}
@@ -66,8 +81,8 @@ async function UserCreate(message, commanderName, verifyDiscordID) {
   			return message.reply(`Thanks, ${commanderName}! Your Discord ID has been added as ${verifyDiscordID}.`);
       }
   } catch(err) {
-    console.log('No existing user ID or discord account, can we try to create one?');
-  		//There's no existing user ID for this name or discord account so let's make one.
+    //console.log('No existing user ID or discord account, can we try to create one?');
+  	//There's no existing user ID for this name or discord account so let's make one.
   	try {
       await sql.query(`
   		  INSERT INTO users (username, discordid)
@@ -75,6 +90,7 @@ async function UserCreate(message, commanderName, verifyDiscordID) {
   		`);
       return message.reply(`Thanks, ${commanderName}! I made you a new account, with Discord ID ${verifyDiscordID}.`);
     } catch(err) {
+      //If all else fails, just apologize and output the error for me.
       console.log('Creating one failed... eek: '+ err);
       return message.reply(`Sorry... I'm kinda freaking out. I hope Zelle is checking my logs.`);
     }
@@ -85,14 +101,15 @@ async function UserCreate(message, commanderName, verifyDiscordID) {
 
 async function PetsCreate(message, args, verifyDiscordID) {
   sql.connect();
-  //check if we know this user
+  //check if we know this user already
+  console.log(verifyDiscordID);
   try {
     const checkUsers = await sql.query(`SELECT userid FROM users WHERE discordid = ${verifyDiscordID}`);
     console.log(checkUsers);
     //we know this guy, let's execute the command
 
     if (!args.length) {
-      //if no arguments let's fetch their Pets first off
+      //if no arguments let's fetch their Pets
         try {
           const checkPets = await sql.query(`SELECT * FROM pets WHERE ownerid = ${checkUsers}`);
           console.log('pets are ' + checkPets);
